163e77f7f2c405b6b8fac1abf32a336e6bae8ecc
korasdor <koras_dor@yahoo.com>
2014-09-03 20:41:02 +0400

    add assets prepeare scene to coding

---
+++ Assets/Scripts/behavs/ButtonBehav.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using AssemblyCSharp;
+using System;
+
+public class ButtonBehav : MonoBehaviour {
+
+	public event EventHandler<ButtonEventArgs> ButtonMouseDown;
+	public event EventHandler<ButtonEventArgs> ButtonMouseUp;
+
+	bool isPressed = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update() {
+
+#if UNITY_ANDROID || UNITY_IPNONE
+		if(Input.touchCount > 0){
+			if (Input.GetTouch(0).phase == TouchPhase.Began) {
+				Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+				RaycastHit hit;
+
+				if (collider.Raycast(ray, out hit, 100.0f)) {
+
+					if(isPressed == false) {
+						OnMouseDown();
+					}
+				}
+			}
+		}
+
+		if(Input.touchCount > 0){
+			if (Input.GetTouch(0).phase == TouchPhase.Ended) {
+				Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+				RaycastHit hit;
+				
+				if (collider.Raycast(ray, out hit, 100.0f)) {
+					if(isPressed == true) {
+						OnMouseUp();
+					}
+				}
+			}
+		}
+#endif
+	}
+
+	void OnMouseDown() {
+		isPressed = true;
+
+		ButtonEventArgs eventArgs = new ButtonEventArgs();
+		eventArgs.ButtonType = gameObject.name;
+
+		ButtonMouseDown(this, eventArgs);
+	}
+
+	void OnMouseUp() {
+		isPressed = false;
+
+		ButtonEventArgs eventArgs = new ButtonEventArgs();
+		eventArgs.ButtonType = gameObject.name;
+		
+		ButtonMouseUp(this, eventArgs);
+	}
+}
